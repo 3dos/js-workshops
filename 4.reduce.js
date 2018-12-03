2681ddefc5249667b07c683768031766dceaa819
filipe.dossantos <filipe.dossantos@gaming1.com>
2018-12-03 16:34:04 +0100

    feature(reduce): implement array exercice

--- 4.reduce.js
+++ 4.reduce.js
@@ -12,3 +12,14 @@ for (const number of numbers) {
 console.log(
   sum // -> 15
 );
+
+// Okay, let's try to implement an Array filter function with reduce
+const filterBy = fn => {
+  // Implement the reducer here
+  // Hint: this should return a reducer function
+};
+const filterArray = (arr, fn) => arr.reduce(filterBy(fn), []);
+
+console.log(
+  filterArray(numbers, number => number % 2 === 0) // -> should output [ 2, 4 ]
+);
